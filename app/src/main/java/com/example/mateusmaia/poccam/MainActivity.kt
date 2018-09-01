ffae2eeffe39085f6a5583f377d75c336a8a511b
mateusmaaia <maieta2122746554@#>
2018-09-01 13:57:48 -0300

    Added gif and background changed

--- app/src/main/java/com/example/mateusmaia/poccam/MainActivity.kt
+++ app/src/main/java/com/example/mateusmaia/poccam/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mateusmaia.poccam
 
 import android.databinding.DataBindingUtil
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.mateusmaia.poccam.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        mBinding = binding
+        binding.look = Look()
+        val handler = Handler()
+
+        Glide.with(this)
+                .load(R.drawable.nonsinal)
+                .into(binding.logo)
+        binding.logo.visibility = View.VISIBLE
 
         binding.acceptButton.setOnClickListener {
             Toast.makeText(this, "To be implemented", Toast.LENGTH_SHORT).show()
